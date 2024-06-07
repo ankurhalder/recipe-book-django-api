3d08d362ba6c5a4a8cfc55cd9ec22594875d3c5d
Ankur Halder <ankur.halder12345@gmail.com>
2024-06-07 19:35:15 +0530

    feat: Add Recipe model to admin site
    This commit adds the Recipe model to the admin site in order to manage recipes through the Django admin interface. The Recipe model is imported in the admin.py file and registered with the admin site using the `admin.site.register()` method.
    
    The Recipe model includes fields for title, ingredients, instructions, and category. It also defines a `__str__()` method to display the title of the recipe in the admin interface.
    
    This change is part of the Django backend implementation for the recipe book.

--- recipe_book/recipes/admin.py
+++ recipe_book/recipes/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+admin.site.register(Recipe)
--- recipe_book/recipes/models.py
+++ recipe_book/recipes/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=100)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
